84425d7a80d0c49e88d873db192f65c8cdf261db
Tuanshan2012 <yc383@outlook.com>
2025-05-13 15:36:41 +0800

    2025.5.13

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -15,9 +15,9 @@ const getBasePath = () => {
 };
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  siteTitle: 'Mr’Luo Running',
+  siteUrl: 'https://mrluoh.github.io/2023/11/07/%E9%83%A8%E7%BD%B2running_page-2023',
+  logo: 'https://s2.loli.net/2023/10/31/KFm1LUv3JIjTXdk.jpg',
   description: 'Personal site and blog',
   navLinks: [
     {
@@ -26,11 +26,11 @@ const data: ISiteMetadataResult = {
     },
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://mrluoh.github.io',
     },
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://mrluoh.github.io/about',
     },
   ],
 };
