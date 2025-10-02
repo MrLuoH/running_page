ce6fea0fb32c317976cf50bd33229ceee746287c
MrLuoH <yc383@outlook.com>
2025-10-02 09:34:00 +0800

    Fix Prettier code style issue in site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,7 +16,8 @@ const getBasePath = () => {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Mr’Luo Running',
-  siteUrl: 'https://mrluoh.github.io/2023/11/07/%E9%83%A8%E7%BD%B2running_page-2023',
+  siteUrl:
+    'https://mrluoh.github.io/2023/11/07/%E9%83%A8%E7%BD%B2running_page-2023',
   logo: 'https://s2.loli.net/2023/10/31/KFm1LUv3JIjTXdk.jpg',
   description: 'Personal site and blog',
   navLinks: [
